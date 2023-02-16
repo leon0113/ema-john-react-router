2e16ef430ff4db5437a14430da1c8a19e4b95da0
leon0113 <tahjib.leon399@gmail.com>
2023-02-17 02:53:06 +0600

    cart delete btn

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,3 +1,5 @@
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './ReviewItem.css';
 
@@ -11,12 +13,14 @@ const ReviewItem = (props) => {
             <div className="item-details-container">
                 <div className="review-details">
                     <p className="product-name" title={name}>{name.length > 20 ? name.slice(0, 20) + '...' : name}</p>
-                    <p className="price">Price : <span className='orange-color'>{price}</span></p>
-                    <p><small>Shipping fee : {shipping}</small></p>
+                    <p className="price">Price : <span className='orange-color'>{price} $</span></p>
+                    <p><small>Shipping fee : {shipping} $</small></p>
                     <p><small>Quantity : {quantity}</small></p>
                 </div>
-                <div className="delete-button">
-                    <button>Delete</button>
+                <div className="delete-container">
+                    <button className='delete-button'>
+                        <FontAwesomeIcon className='delete-icon' icon={faTrashAlt}></FontAwesomeIcon>
+                    </button>
                 </div>
             </div>
         </div>
