26342bccaa51dc50e38657e8e54c6c90762538bb
leon0113 <tahjib.leon399@gmail.com>
2023-02-18 20:24:02 +0600

    done

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
 import { removeFromDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
+import './orders.css'
 
 const Orders = () => {
     const [products, setProducts] = useProducts();
     const [cart, setCart] = useCart(products);
+    const navigate = useNavigate();
+
 
     const handleRemoveProduct = product => {
         const rest = cart.filter(pd => pd.id !== product.id);
@@ -27,7 +31,11 @@ const Orders = () => {
                 }
             </div>
             <div className="cart-container">
-                <Cart cart={cart}></Cart>
+                <Cart cart={cart}>
+
+                    <button onClick={() => navigate('/inventory')}>Procced checkout</button>
+
+                </Cart>
             </div>
         </div>
     );
